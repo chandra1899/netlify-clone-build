446e6e6a3029fb3e7960bc95f5cb33063694dbc4
chandra <101719497+chandra1899@users.noreply.github.com>
2024-06-12 18:20:48 +0530

    removes secrets from commits

--- dist/index.js
+++ dist/index.js
@@ -12,12 +12,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+// import { createClient, commandOptions } from "redis";
 const aws_1 = require("./aws");
 const utils_1 = require("./utils");
 const updatestatus_1 = require("./updatestatus");
 const deleteFolder_1 = require("./deleteFolder");
 const path_1 = __importDefault(require("path"));
 const ioredis_1 = __importDefault(require("ioredis"));
+// const publisher = createClient()
+// publisher.connect()
+// const subscriber = createClient()
+// subscriber.connect()
 const publisher = new ioredis_1.default({
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT),
@@ -37,7 +43,7 @@ function main() {
             // @ts-ignore
             const id = res[1];
             yield (0, aws_1.downloadS3Folder)(`output/${id}`);
-            console.log("downloded");
+            // console.log("downloded");
             yield (0, updatestatus_1.updatestatus)(id, "building");
             yield publisher.hset("status", id, "building...");
             yield (0, utils_1.buildproject)(id);
@@ -45,10 +51,10 @@ function main() {
             yield publisher.hset("status", id, "build...");
             yield publisher.hset("status", id, "deploying...");
             yield (0, aws_1.copyFinalDist)(id);
-            console.log("deleting files");
-            yield (0, deleteFolder_1.deleteFolder)(path_1.default.join(__dirname, `output/${id}`));
-            console.log("deleted all files");
             yield (0, updatestatus_1.updatestatus)(id, "deployed");
+            // console.log("deleting files");
+            yield (0, deleteFolder_1.deleteFolder)(path_1.default.join(__dirname, `output/${id}`));
+            // console.log("deleted all files");
             yield publisher.hset("status", id, "deployed");
         }
     });
